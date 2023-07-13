7ebd9746a0f5c2020f37fbf97da22d41719ecb85
Dior2004 <kupaysinovdiyorbek@gmail.com>
2023-07-14 04:10:54 +0500

    browser compatibility

--- main.js
+++ main.js
@@ -2,11 +2,43 @@ let localStream;
 let remoteStream;
 
 let getLocalStream = async () => {
-  localStream = await navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: false,
-  });
-  document.getElementById("bgPlay").srcObject = localStream;
+  try {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      // Standard API
+      localStream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: false,
+      });
+    } else if (navigator.mozGetUserMedia) {
+      // Firefox-specific API
+      localStream = await new Promise((resolve, reject) => {
+        navigator.mozGetUserMedia(
+          {
+            video: true,
+            audio: false,
+          },
+          resolve,
+          reject
+        );
+      });
+    } else if (navigator.webkitGetUserMedia) {
+      // WebKit (Safari) specific API
+      localStream = await new Promise((resolve, reject) => {
+        navigator.webkitGetUserMedia(
+          {
+            video: true,
+            audio: false,
+          },
+          resolve,
+          reject
+        );
+      });
+    }
+
+    document.getElementById("bgPlay").srcObject = localStream;
+  } catch (error) {
+    console.log("Error accessing camera:", error);
+  }
 };
 
 getLocalStream();
