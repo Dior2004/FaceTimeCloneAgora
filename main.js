65e45e488f55a69248841e115fb4f472278f4ab5
Dior2004 <kupaysinovdiyorbek@gmail.com>
2023-07-13 12:56:17 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+let localStream;
+let remoteStream;
+
+let getLocalStream = async () => {
+  localStream = await navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: false,
+  });
+  document.getElementById("bgPlay").srcObject = localStream;
+};
+
+getLocalStream();
