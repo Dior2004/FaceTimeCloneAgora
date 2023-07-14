d8d4b3e35e70e06485f019fc55227c74e2832905
Dior2004 <kupaysinovdiyorbek@gmail.com>
2023-07-14 05:43:48 +0500

    link copy added

--- main.js
+++ main.js
@@ -11,3 +11,18 @@ let getLocalStream = async () => {
 };
 
 getLocalStream();
+
+function copyUrlToClipboard() {
+  var input = document.createElement("input");
+
+  input.value = window.location.href;
+
+  document.body.appendChild(input);
+  input.select();
+  document.execCommand("copy");
+  document.body.removeChild(input);
+}
+
+document
+  .getElementById("shareLink")
+  .addEventListener("click", copyUrlToClipboard);
