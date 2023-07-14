38bf79d5a994d2eaef31ab71ab7a08a1d58b3ab5
Dior2004 <kupaysinovdiyorbek@gmail.com>
2023-07-15 01:27:31 +0500

    changes

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+let shareLink = document.getElementById("shareLink");
+let buttonContent = document.getElementById("buttonContent");
+let videosSection = document.querySelector(".videos");
+
+buttonContent.innerText = "Create Link";
+
 let localStream;
 let remoteStream;
 
@@ -21,8 +27,16 @@ function copyUrlToClipboard() {
   input.select();
   document.execCommand("copy");
   document.body.removeChild(input);
+
+  buttonContent.innerText = "Created";
+  shareLink.style = "background-color: #00f731b3";
+
+  setTimeout(() => {
+    videosSection.style = "scale: 1; transition: 0.5s;";
+    setTimeout(() => {
+      videosSection.style = "scale: 1; border-radius: 0px; transition: 0.5s;";
+    }, 400);
+  }, 500);
 }
 
-document
-  .getElementById("shareLink")
-  .addEventListener("click", copyUrlToClipboard);
+shareLink.addEventListener("click", copyUrlToClipboard);
