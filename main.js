820a685a395bd859cecdcc4caccbe3bba519b115
Dior2004 <kupaysinovdiyorbek@gmail.com>
2023-07-19 03:37:47 +0500

    customisation started

--- main.js
+++ main.js
@@ -1,72 +1,18 @@
 let shareLink = document.getElementById("shareLink");
-let buttonContent = document.getElementById("buttonContent");
 let videosSection = document.querySelector(".videos");
 let myVideo = document.getElementById("myVideo");
 let peersVideo = document.getElementById("peersVideo");
 
-buttonContent.innerText = "Create Link";
-
-let APP_ID = "62c1bcd773ea4592bb4f0f5ff8ad6b2e";
-let token = null;
-let uid = Math.floor(Math.random() * 10000);
-
-// client object that we login with
-let client;
-// this is the channel where two users join together
-let channel;
-
-let localStream;
-let remoteStream;
-let peerConnection;
-
-const servers = {
-  iceServers: [
-    { urls: [`stun:stun1.l.google.com19302`, `stun:stun2.l.google.com19302`] },
-  ],
-};
-
 let init = async () => {
   localStream = await navigator.mediaDevices.getUserMedia({
     video: true,
-    audio: false,
+    audio: true,
   });
 
   document.getElementById("bgPlay").srcObject = localStream;
-  myVideo.srcObject = localStream;
-
-  createOffer();
 };
 
 init();
-
-let createOffer = async () => {
-  peerConnection = new RTCPeerConnection(servers);
-
-  remoteStream = new MediaStream();
-  peersVideo.srcObject = remoteStream;
-
-  localStream.getTracks().forEach((track) => {
-    peerConnection.addTrack(track, localStream);
-  });
-
-  peerConnection.ontrack = (event) => {
-    event.streams[0].getTracks().forEach((track) => {
-      remoteStream.addTrack();
-    });
-  };
-
-  peerConnection.onicecandidate = async (event) => {
-    if (event.candidate) {
-      console.log(event.candidate);
-    }
-  };
-
-  let offer = await peerConnection.createOffer();
-  await peerConnection.setLocalDescription(offer);
-  console.log(offer);
-};
-
-// link sharing
 
 function copyUrlToClipboard() {
   var input = document.createElement("input");
@@ -78,7 +24,7 @@ function copyUrlToClipboard() {
   document.execCommand("copy");
   document.body.removeChild(input);
 
-  buttonContent.innerText = "Created";
+  document.getElementById("buttonContent").innerText = "Created";
   shareLink.style = "background-color: #00f731b3";
 
   setTimeout(() => {
@@ -90,3 +36,128 @@ function copyUrlToClipboard() {
 }
 
 shareLink.addEventListener("click", copyUrlToClipboard);
+
+// create Agora client
+var client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
+
+var localTracks = {
+  videoTrack: null,
+  audioTrack: null,
+};
+var remoteUsers = {};
+// Agora client options
+var options = {
+  appid: "62c1bcd773ea4592bb4f0f5ff8ad6b2e",
+  channel: null,
+  uid: null,
+  token: null,
+};
+
+// the demo can auto join channel with params in url
+window.addEventListener("DOMContentLoaded", function () {
+  var urlParams = new URL(location.href).searchParams;
+  options.appid = urlParams.get("appid");
+  options.channel = urlParams.get("channel");
+  options.token = urlParams.get("token");
+  if (options.channel) {
+    document.getElementById("appid").value = options.appid;
+    document.getElementById("channel").value = options.channel;
+    document.getElementById("join-form").submit();
+  }
+});
+
+document
+  .getElementById("join-form")
+  .addEventListener("submit", async function (e) {
+    e.preventDefault();
+    document.getElementById("join").disabled = true;
+    try {
+      options.appid = "62c1bcd773ea4592bb4f0f5ff8ad6b2e";
+      options.channel = document.getElementById("channel").value;
+      await join();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      document.getElementById("leave").disabled = false;
+    }
+  });
+
+document.getElementById("leave").addEventListener("click", function () {
+  leave();
+});
+
+async function join() {
+  // add event listener to play remote tracks when remote user publishes.
+  client.on("user-published", handleUserPublished);
+  client.on("user-unpublished", handleUserUnpublished);
+
+  // join a channel and create local tracks, we can use Promise.all to run them concurrently
+  [options.uid, localTracks.audioTrack, localTracks.videoTrack] =
+    await Promise.all([
+      // join the channel
+      client.join(options.appid, options.channel, options.token || null),
+      // create local tracks, using microphone and camera
+      AgoraRTC.createMicrophoneAudioTrack(),
+      AgoraRTC.createCameraVideoTrack(),
+    ]);
+
+  // play local video track
+  localTracks.videoTrack.play("local-player");
+
+  // publish local tracks to channel
+  await client.publish(Object.values(localTracks));
+  console.log("publish success");
+}
+
+async function leave() {
+  for (var trackName in localTracks) {
+    var track = localTracks[trackName];
+    if (track) {
+      track.stop();
+      track.close();
+      localTracks[trackName] = undefined;
+    }
+  }
+
+  // remove remote users and player views
+  remoteUsers = {};
+
+  // leave the channel
+  await client.leave();
+
+  document.getElementById("join").disabled = false;
+  document.getElementById("leave").disabled = true;
+  console.log("client leaves channel success");
+}
+
+async function subscribe(user, mediaType) {
+  const uid = user.uid;
+  // subscribe to a remote user
+  await client.subscribe(user, mediaType);
+  console.log("subscribe success");
+  if (mediaType === "video") {
+    const playerWrapper = document.createElement("div");
+    playerWrapper.id = `player-wrapper-${uid}`;
+    playerWrapper.innerHTML = `
+      <p class="player-name">remoteUser(${uid})</p>
+      <div id="player-${uid}" class="player"></div>
+    `;
+    document.getElementById("remote-playerlist").appendChild(playerWrapper);
+    user.videoTrack.play(`player-${uid}`);
+  }
+  if (mediaType === "audio") {
+    user.audioTrack.play();
+  }
+}
+
+function handleUserPublished(user, mediaType) {
+  const id = user.uid;
+  remoteUsers[id] = user;
+  subscribe(user, mediaType);
+}
+
+function handleUserUnpublished(user) {
+  const id = user.uid;
+  delete remoteUsers[id];
+  document.getElementById(`player-wrapper-${id}`).remove();
+}
