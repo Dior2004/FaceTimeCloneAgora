e1b2aa9f271bceb92ad05a4cb36f3e1af9e9b6fc
Dior2004 <kupaysinovdiyorbek@gmail.com>
2023-07-19 05:25:56 +0500

    one more time

--- main.js
+++ main.js
@@ -31,12 +31,12 @@ function createFaceTime() {
   } else {
   }
 
-  // setTimeout(() => {
-  //   videosSection.style = "scale: 1; transition: 0.5s;";
-  //   setTimeout(() => {
-  //     videosSection.style = "scale: 1; border-radius: 0px; transition: 0.5s;";
-  //   }, 400);
-  // }, 500);
+  setTimeout(() => {
+    videosSection.style = "scale: 1; transition: 0.5s;";
+    setTimeout(() => {
+      videosSection.style = "scale: 1; border-radius: 0px; transition: 0.5s;";
+    }, 400);
+  }, 500);
 }
 
 createNewFaceTime.addEventListener("click", createFaceTime);
